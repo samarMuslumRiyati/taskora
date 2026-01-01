58ef256ac4804186c103fe96331ac06d3d0eec81
samar riyati <samarriyati.muslum@gmail.com>
2026-01-01 15:11:11 +0200

    feat: setup icons & images paths

---
+++ lib/core/config/constants/icon_path.dart
@@ -0,0 +1,22 @@
+
+
+class IconPath {
+
+
+  IconPath._();
+
+
+  static const main = 'assets/icons/main.svg';
+  static const person = 'assets/icons/person.svg';
+  static const projects = 'assets/icons/projects.svg';
+
+   static const colordMain = 'assets/icons/colordMain.svg';
+  static const colordPerson = 'assets/icons/colordPerson.svg';
+  static const colordProjects = 'assets/icons/colordProjects.svg';
+
+
+  static const arrow = 'assets/icons/arrow.svg';
+  static const email = 'assets/icons/email.svg';
+
+
+}
---
+++ lib/core/config/constants/image_path.dart
@@ -0,0 +1,9 @@
+class ImagePath {
+  ImagePath._();
+
+  static const String appLogo = 'assets/images/app_logo.png';
+  static const String noDataImage = 'assets/images/noDataImage.png';
+  static const String successOk = 'assets/images/successOk.PNG';
+
+
+}
