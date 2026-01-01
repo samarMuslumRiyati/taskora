daca97aa1c5696d2ede59679cc55febe7e8470b1
samar riyati <samarriyati.muslum@gmail.com>
2026-01-01 15:20:53 +0200

    feat: setup app elevated button widget

---
+++ lib/core/config/widgets/app_elevated_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppElevatedButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const AppElevatedButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(
+        text,
+      ),
+    );
+  }
+}
