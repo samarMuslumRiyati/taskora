57f9d7fb5844dd0fa2f5a6c779178e4e2c9fbeeb
samar riyati <samarriyati.muslum@gmail.com>
2026-01-01 15:25:06 +0200

    feat: prepare some app strings

---
+++ lib/core/config/constants/app_strings.dart
@@ -0,0 +1,46 @@
+class AppStrings {
+  AppStrings._();
+
+  // App Name
+  static const String appName = 'Taskora';
+  static const String person = 'الملف الشخصي';
+  static const String projects = 'المشاريع';
+  static const String mainText = 'الرئيسية';
+
+
+
+
+
+  static const String createAcc = 'إنشاء الحساب';
+  static const String next = 'التالي';
+
+  static const String login = 'تسجيل الدخول';
+  static const String enterEmail = 'أدخل بريديك الإلكتروني';
+
+
+
+  static const String yes = 'نعم';
+  static const String no = 'لا';
+
+
+
+
+
+
+
+  static const String doNotHaveAcc = 'اذا يوجد لديك حساب؟';
+  static const String createAccNow = ' قم بإنشاء حساب';
+
+
+
+  // onboarding texts
+static const onboardingText1 = 'إدارة مشاريعك بكل سهولة ووضوح';
+  static const onboardingText2 = 'أنشئ مشاريعك، حدّد تفاصيلها، وتابع تقدمك خطوة بخطوة في مكان واحد مصمّم خصيصاً للمستقلين.';
+
+
+
+  static const areYouSureToDeleteTask = 'هل أنت متأكد أنك تريد\nحذف هذه المهمة؟';
+
+
+
+}
